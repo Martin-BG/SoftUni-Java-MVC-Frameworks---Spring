6ca92ba7c10e414b32132024b446ada5590ebe24
MartinBG <MartinBG@abv.bg>
2018-07-12 12:24:25 +0300

    Use of base-layout for all views, extended BaseController

--- 03. Java MVC Frameworks - Thymeleaf Engine/Resident Evil/src/main/java/com/softuni/residentevil/controllers/BaseController.java
+++ 03. Java MVC Frameworks - Thymeleaf Engine/Resident Evil/src/main/java/com/softuni/residentevil/controllers/BaseController.java
@@ -4,14 +4,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 public abstract class BaseController {
 
-    public ModelAndView view(String viewName, ModelAndView modelAndView) {
-        modelAndView.setViewName(viewName);
+    private static final String DEFAULT_TITLE = "Viruses";
+
+    public ModelAndView view(String viewName, Object viewModel, String title) {
+        if (title == null) {
+            title = DEFAULT_TITLE;
+        }
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/fragments/base-layout");
+        modelAndView.addObject("viewName", viewName);
+        modelAndView.addObject("viewModel", viewModel);
+        modelAndView.addObject("title", title);
 
         return modelAndView;
     }
 
+    public ModelAndView view(String viewName, String title) {
+        return this.view(viewName, null, title);
+    }
+
+    public ModelAndView view(String viewName, Object viewModel) {
+        return this.view(viewName, viewModel, null);
+    }
+
     public ModelAndView view(String viewName) {
-        return this.view(viewName, new ModelAndView());
+        return this.view(viewName, null, null);
     }
 
     public ModelAndView redirect(String redirectUrl) {
