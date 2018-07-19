f9e89ded041618e0a8728b87801408a7d6ab7183
MartinBG <MartinBG@abv.bg>
2018-07-19 18:16:40 +0300

    exercise - task

--- 02. Java MVC Frameworks - Spring Essentials/Car Dealer/src/main/java/org/softuni/cardealer/controllers/SalesController.java
+++ 02. Java MVC Frameworks - Spring Essentials/Car Dealer/src/main/java/org/softuni/cardealer/controllers/SalesController.java
@@ -3,7 +3,9 @@ package org.softuni.cardealer.controllers;
 import org.softuni.cardealer.services.CarService;
 import org.softuni.cardealer.utils.MessageWrapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/sales")
@@ -12,5 +14,10 @@ public class SalesController extends BaseController {
     protected SalesController(final MessageWrapper messageWrapper,
                               final CarService carService) {
         super(messageWrapper, carService);
+    }
+
+    @GetMapping(value = {"", "/"})
+    public ModelAndView rootGet() {
+        return super.view("/sales/all");
     }
 }
